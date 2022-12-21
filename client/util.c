907422a8d363c878b9113a2a1e0d7790e405e549
Cody <celdond@gmail.com>
2022-12-20 21:19:15 -0800

    added node insertion

--- client/util.c
+++ client/util.c
@@ -10,3 +10,16 @@ struct link_list *create_list() {
 
 	return l;
 }
+
+struct node *insert_node(struct node *n) {
+	struct node *new = (struct node *)calloc(1, sizeof(struct node));
+        if (!new) {
+                return ((struct node *) NULL);
+        }
+	new->command = '0';
+	new->file_name = NULL;
+	new->next = NULL;
+
+	n->next = new;
+	return new;
+}
