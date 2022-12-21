00fe4205d452ca2f2d75d0fdf4004294843d8290
Cody <celdond@gmail.com>
2022-12-20 22:12:55 -0800

    added delete functions for link list

--- client/util.c
+++ client/util.c
@@ -23,5 +23,30 @@ struct node *insert_node(struct link_list *l) {
 
 	l->tail->next = new;
 	l->tail = new;
+	l->length += 1;
+
 	return new;
+}
+
+void delete_node(struct node *n) {
+	if (n->file_name) {
+		free(n->file_name);
+	}
+	free(n);
+	n = NULL;
+	return;
+}
+
+void delete_list(struct link_list *l) {
+	int node_count = l->length;
+	struct node *n = l->head;
+	struct next = n->next;
+
+	for (int i = 0; i < node_count; i++) {
+		delete_node(n);
+		n = next;
+		next = n->next;
+	}
+	free(l);
+	return;
 }
