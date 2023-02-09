3587c85451c6face6f25d6671d19d8bfd57d7466
Cody <celdond@gmail.com>
2023-02-08 23:34:18 -0800

    added pthread core code to client-side

---
+++ client/drone.h
@@ -0,0 +1,24 @@
+#ifndef __DRONE_H__
+#define __DRONE_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+struct threa *thread_op;
+
+pthread_t *thread_list;
+
+struct threa {
+	int *work_buffer;
+	int in;
+	int out;
+	int count;
+	int thread_count;
+	int max_count;
+};
+
+struct threa *create_thread_sheet(int thread_count, int max_count);
+
+void free_threa(struct threa *t);
+#endif
